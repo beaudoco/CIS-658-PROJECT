7c480caceedb90d2e19a2df74aa1f5a8d59364b4
beaudoco <beaudoco@mail.gvsu.edu>
2020-03-29 15:56:38 -0400

    working on saving new shows

--- src/community_add_show_component.js
+++ src/community_add_show_component.js
@@ -2,10 +2,6 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItem from '@material-ui/core/ListItem';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
@@ -13,6 +9,10 @@ import Typography from '@material-ui/core/Typography';
 import CloseIcon from '@material-ui/icons/Close';
 import Slide from '@material-ui/core/Slide';
 import './community_add_show_component.css';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import * as firebase from 'firebase';
 
 const useStyles = makeStyles(theme => ({
   appBar: {
@@ -28,47 +28,117 @@ const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
-export default function FullScreenDialog() {
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
+export class FullScreenDialog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    }
+  }
 
-  const handleClickOpen = () => {
-    setOpen(true);
+  handleClickOpen() {
+    this.setState({
+      open: true
+    });
   };
 
-  const handleClose = () => {
-    setOpen(false);
+  handleClose() {
+    this.setState({
+      open: false
+    });
   };
 
-  return (
-    <div>
-      <Button className="addShow" color="primary" onClick={handleClickOpen}>
-        Add Series
-      </Button>
-      <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
-        <AppBar className={classes.appBar}>
-          <Toolbar>
-            <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
-              <CloseIcon />
-            </IconButton>
-            <Typography variant="h6" className={classes.title}>
-              Add New Series
-            </Typography>
-            <Button autoFocus color="inherit" onClick={handleClose}>
-              save
-            </Button>
-          </Toolbar>
-        </AppBar>
-        <List>
-          <ListItem button>
-            <ListItemText primary="Phone ringtone" secondary="Titania" />
-          </ListItem>
-          <Divider />
-          <ListItem button>
-            <ListItemText primary="Default notification ringtone" secondary="Tethys" />
-          </ListItem>
-        </List>
-      </Dialog>
-    </div>
-  );
+  handleSave() {
+    const rootRef = firebase.firestore().collection('shows');
+    rootRef.add({
+      providerID: this.props.providerID,
+      showDescription: document.getElementById('show-description').value,
+      showID: Date.now() + Math.random(),
+      showImage: document.getElementById('show-image').value,
+      showTitle: document.getElementById('show-title').value
+    });
+
+    this.setState({
+      open: false
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Button className="addShow" color="primary" onClick={() => this.handleClickOpen()}>
+          Add Series
+        </Button>
+        <Dialog fullScreen open={this.state.open} onClose={() => this.handleClose()} TransitionComponent={Transition}>
+          <AppBar className="appBar">
+            <Toolbar>
+              <IconButton edge="start" color="inherit" onClick={() => this.handleClose()} aria-label="close">
+                <CloseIcon />
+              </IconButton>
+              <Typography variant="h6" className="title">
+                Add New Series
+              </Typography>
+              <Button autoFocus color="inherit" onClick={() => this.handleSave()}>
+                save
+              </Button>
+            </Toolbar>
+          </AppBar>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <div className="root">
+            <Grid container spacing={2}>
+              <Grid item xs>
+                <Paper className="paper">
+                  <TextField
+                    id="show-title"
+                    label="Show Title"
+                    fullWidth
+                    margin="normal"
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    variant="outlined"
+                  />
+                </Paper>
+              </Grid>
+              <Grid item xs>
+                <Paper className="paper">
+                  <TextField
+                    id="show-description"
+                    label="Show Description"
+                    fullWidth
+                    margin="normal"
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    variant="outlined"
+                  />
+                </Paper>
+              </Grid>
+              <Grid item xs>
+                <Paper className="paper">
+                  <TextField
+                    id="show-image"
+                    label="Show Image"
+                    fullWidth
+                    margin="normal"
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    variant="outlined"
+                  />
+                </Paper>
+              </Grid>
+            </Grid>
+          </div>
+        </Dialog>
+      </div>
+    );
+  }
+
 }
+
+export default FullScreenDialog;
